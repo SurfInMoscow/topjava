bd169ad10ffe39f827094f0965db809e4278171f
Pavel Vorobev <p.vorobyev@inbox.ru>
2019-02-03 03:32:12 +0300

    HW0_update

--- src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/UserMealsUtil.java
@@ -19,11 +19,12 @@ public class UserMealsUtil {
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 13, 0), "Обед", 500),
                 new UserMeal(LocalDateTime.of(2015, Month.MAY, 31, 20, 0), "Ужин", 510)
         );
-        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(12, 0), 2000);
+        getFilteredWithExceeded(mealList, LocalTime.of(7, 0), LocalTime.of(21, 0), 2000);
     }
 
     public static List<UserMealWithExceed> getFilteredWithExceeded(List<UserMeal> mealList, LocalTime startTime, LocalTime endTime, int caloriesPerDay) {
         List<UserMealWithExceed> userMealWithExceeds;
+        Set<String> setExceed = new TreeSet<>();
         Map<String, Integer> map = new HashMap<>();
         userMealWithExceeds = mealList.stream().map(p -> {
             boolean exceed = false;
@@ -32,18 +33,28 @@ public class UserMealsUtil {
                     int countCalories = map.get(TimeUtil.toLocalDate(p.getDateTime()).toString()) + p.getCalories();
                     if (countCalories > caloriesPerDay) {
                         exceed = true;
+                        setExceed.add(TimeUtil.toLocalDate(p.getDateTime()).toString());
                     }
                     map.put(TimeUtil.toLocalDate(p.getDateTime()).toString(), countCalories);
                 } else {
                     map.put(TimeUtil.toLocalDate(p.getDateTime()).toString(), p.getCalories());
                     if (p.getCalories() > caloriesPerDay) {
                         exceed = true;
+                        setExceed.add(TimeUtil.toLocalDate(p.getDateTime()).toString());
                     }
                 }
                 return new UserMealWithExceed(p.getDateTime(), p.getDescription(), p.getCalories(), exceed);
             }
             return null;
         }).collect(Collectors.toList()).stream().filter(Objects::nonNull).collect(Collectors.toList());
+        userMealWithExceeds.forEach(m -> {
+            setExceed.forEach(s -> {
+                if (TimeUtil.toLocalDate(m.getDateTime()).toString().equals(s)) {
+                    m.setExceed(true);
+                }
+            });
+        });
+        System.out.println(userMealWithExceeds);
         return userMealWithExceeds;
     }
 }
