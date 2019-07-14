8ab0c9886bac1d3a1e94dc76c3f8dffd7f0079ac
Pavel Vorobev <p.vorobyev@inbox.ru>
2019-07-14 13:22:53 +0300

    4_2_HW3_fix_logging.patch

--- src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
+++ src/test/java/ru/javawebinar/topjava/service/MealServiceTest.java
@@ -24,12 +24,6 @@ import static ru.javawebinar.topjava.MealTestData.*;
 @Sql(scripts = "classpath:db/populateDB.sql", config = @SqlConfig(encoding = "UTF-8"))
 public class MealServiceTest {
 
-    static {
-        // Only for postgres driver logging
-        // It uses java.util.logging and logged via jul-to-slf4j bridge
-        SLF4JBridgeHandler.install();
-    }
-
     @Autowired
     private MealService service;
 
