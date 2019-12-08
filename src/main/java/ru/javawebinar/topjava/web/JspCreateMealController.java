3970b2b81110791547545f3d6892e7977c0303cc
Pavel Vorobev <p.vorobyev@inbox.ru>
2019-12-08 23:25:12 +0300

    HW06 mapping update

--- src/main/java/ru/javawebinar/topjava/web/JspCreateMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/JspCreateMealController.java
@@ -1,12 +1,12 @@
 package ru.javawebinar.topjava.web;
 
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class JspCreateMealController {
-    @GetMapping("/createMeal")
+
     protected static String createMeal() {
         return "createMeal";
     }
+
 }
--- src/main/java/ru/javawebinar/topjava/web/JspEditMealController.java
+++ src/main/java/ru/javawebinar/topjava/web/JspEditMealController.java
@@ -1,12 +1,9 @@
 package ru.javawebinar.topjava.web;
 
-import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
-
-@Controller
 public class JspEditMealController {
-    @GetMapping("/editMeal")
+
     protected static String editMeal() {
         return "editMeal";
     }
+
 }
