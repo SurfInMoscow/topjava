acc42dfa58c535939f2aca3acf1a6020fd611312
Pavel Vorobev <p.vorobyev@inbox.ru>
2019-12-10 13:29:20 +0300

    7_01_HW6_fix_tests.patch improvements for jpa user test with JpaUtil

--- src/test/java/ru/javawebinar/topjava/service/datajpa/UserServiceDataJPATest.java
+++ src/test/java/ru/javawebinar/topjava/service/datajpa/UserServiceDataJPATest.java
@@ -1,27 +1,17 @@
 package ru.javawebinar.topjava.service.datajpa;
 
 import org.junit.Assert;
-import org.junit.Before;
 import org.junit.Test;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
 import ru.javawebinar.topjava.model.User;
-import ru.javawebinar.topjava.repository.JpaUtil;
-import ru.javawebinar.topjava.service.AbstractUserServiceTest;
+import ru.javawebinar.topjava.service.AbstractJpaUserServiceTest;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
 
 import static ru.javawebinar.topjava.Profiles.DATAJPA;
 import static ru.javawebinar.topjava.UserTestData.*;
 
 @ActiveProfiles(DATAJPA)
-public class UserServiceDataJPATest extends AbstractUserServiceTest {
-    @Autowired
-    protected JpaUtil jpaUtil;
-
-    @Before
-    public void clear2ndLevelCache() throws Exception {
-        jpaUtil.clear2ndLevelHibernateCache();
-    }
+public class UserServiceDataJPATest extends AbstractJpaUserServiceTest {
 
     @Test
     public void testGetWithMeals() throws Exception {
--- src/test/java/ru/javawebinar/topjava/service/jpa/UserServiceJPATest.java
+++ src/test/java/ru/javawebinar/topjava/service/jpa/UserServiceJPATest.java
@@ -1,20 +1,11 @@
 package ru.javawebinar.topjava.service.jpa;
 
-import org.junit.Before;
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.test.context.ActiveProfiles;
-import ru.javawebinar.topjava.repository.JpaUtil;
-import ru.javawebinar.topjava.service.AbstractUserServiceTest;
+import ru.javawebinar.topjava.service.AbstractJpaUserServiceTest;
 
 import static ru.javawebinar.topjava.Profiles.JPA;
 
 @ActiveProfiles(JPA)
-public class UserServiceJPATest extends AbstractUserServiceTest {
-    @Autowired
-    protected JpaUtil jpaUtil;
+public class UserServiceJPATest extends AbstractJpaUserServiceTest {
 
-    @Before
-    public void clear2ndLevelCache() throws Exception {
-        jpaUtil.clear2ndLevelHibernateCache();
-    }
 }
