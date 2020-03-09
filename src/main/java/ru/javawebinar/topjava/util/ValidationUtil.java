b98f5207ad687752aee3c6398738f3281e3ff29a
Pavel Vorobev <p.vorobyev@inbox.ru>
2020-03-09 19:40:52 +0300

    10_01_HW9_binding_ajax.patch

--- src/main/java/ru/javawebinar/topjava/util/ValidationUtil.java
+++ src/main/java/ru/javawebinar/topjava/util/ValidationUtil.java
@@ -1,8 +1,10 @@
 package ru.javawebinar.topjava.util;
 
-
+import org.springframework.http.ResponseEntity;
+import org.springframework.validation.BindingResult;
 import ru.javawebinar.topjava.HasId;
 import ru.javawebinar.topjava.util.exception.NotFoundException;
+import java.util.stream.Collectors;
 
 public class ValidationUtil {
 
@@ -52,5 +54,13 @@ public class ValidationUtil {
             result = cause;
         }
         return result;
+    }
+
+    public static ResponseEntity<String> getErrorResponse(BindingResult result) {
+        return ResponseEntity.unprocessableEntity().body(
+                result.getFieldErrors().stream()
+                        .map(fe -> String.format("[%s] %s", fe.getField(), fe.getDefaultMessage()))
+                        .collect(Collectors.joining("<br>"))
+        );
     }
 }
